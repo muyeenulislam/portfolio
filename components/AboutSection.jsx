05ddf2a2c73836d9fda145e89d854177d245bd02
muyeen <muyeenulislamsakif@gmail.com>
2023-10-19 14:55:27 +0600

    about

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -19,7 +19,7 @@ const AboutSection = () => {
   };
   return (
     <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           className="rounded-[100px]"
           src={"/images/aboutPhoto.jpeg"}
