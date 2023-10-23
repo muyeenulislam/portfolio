c24a32fa006bde2dc5569f032f3b835fa49835eb
muyeen <muyeenulislamsakif@gmail.com>
2023-10-23 11:53:37 +0600

    footer

---
+++ components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className="footer z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white flex justify-center">
+      <div className="container p-12 flex justify-between">
+        <Image
+          className="rounded-full"
+          src={"/images/navIcon.jpg"}
+          alt="navIcon"
+          height={50}
+          width={50}
+        />
+        <p className="text-slate-600 flex items-center">All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
